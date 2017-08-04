7e7e94b1297a7f6a5cb1efbd6eea499c8ca25f32
Mac User <sabbey37@gmail.com>
2017-08-04 17:55:58 -0400

    Function that filters through file names in a given path, matches only file names with requested extension

--- filtered.js
+++ filtered.js
@@ -2,14 +2,33 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdir(process.argv[2], "utf8", (err, buffer) => {
+    if (err) {
+        console.log(err.message);
+    }
     let files = buffer.toString();
-    files.filter(() => {
-        pathFil(file, process.argv[3]);
+    files = files.split(',');
+   
+    let newFiles = files.filter((file) => {
+        var ext = "." + process.argv[3];
+        return path.extname(file) === ext;
+    })
+    newFiles.forEach((element) => {
+        console.log(element);
     });
 });
 
-function pathFil(file, ext) {
-    var dot = ".";
-    ext = dot + ext;
-    return path.extname(file) === ext
-}
+// Example solution
+// var fs = require('fs')
+//     var path = require('path')
+    
+//     var folder = process.argv[2]
+//     var ext = '.' + process.argv[3]
+    
+//     fs.readdir(folder, function (err, files) {
+//       if (err) return console.error(err)
+//       files.forEach(function (file) {
+//         if (path.extname(file) === ext) {
+//           console.log(file)
+//         }
+//       })
+//     })
