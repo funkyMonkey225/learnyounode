8e8e8f8c65a9e3910f4b1733e5a39810b9df0fc9
Mac User <sabbey37@gmail.com>
2017-08-05 13:54:04 -0400

    Edit collect.js file

--- collect.js
+++ collect.js
@@ -1,14 +1,14 @@
 // Collects all data from server via HTTP GET request
-var http = require('http')
-var url = process.argv[2]
-var body = ''
+var http = require('http');
+var url = process.argv[2];
+var body = '';
  
 http.get(url, function (response) {
   response.on('data', function (data) {
-    body += data
+    body += data;
   })
   response.on('end', function () {
-    console.log(body.length)
-    console.log(body)
+    console.log(body.length);
+    console.log(body);
   })
 })
